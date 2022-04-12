b049a137e31c39fb59c0d6c0311cda8ff0542dab
Bakhromoff <hotamtoy9007@gmail.com>
2022-04-12 14:25:39 +0500

    add belgium.html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
 const elDarkModeButton= document.querySelector('.dark-mode-button');
 elDarkModeButton.addEventListener('click', function () {
-  document.body.classList.add('dark-mode')
-  document.header.classList.add('dark-mode')
-  document.input.classList.add('dark-mode')
-  document.querySelector(option).classList.add('dark-mode')
+  document.body.classList.toggle('dark-mode')
 })
-// elDarkModeButton.addEventListener('click', function () {
-//   document.body.classList.remove('dark-mode')
-// })
